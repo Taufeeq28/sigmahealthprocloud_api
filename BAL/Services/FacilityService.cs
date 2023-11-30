86f639dfc1bf7ce51c42d3482d751aa999f26789
Quasif <quasif92@outlook.com>
2023-11-30 20:15:27 +0530

    merge with Main

---
+++ BAL/Services/FacilityService.cs
@@ -0,0 +1,16 @@
+﻿using BAL.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Services
+{
+    public class FacilityService:IFacilityService
+    {
+        public FacilityService() { }
+
+
+    }
+}
---
+++ Web_API/Controllers/FacilityController.cs
@@ -0,0 +1,31 @@
+﻿using BAL.Interface;
+using BAL.Request;
+using Data.Models;
+using Data.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacilityController : ControllerBase
+    {
+        private IFacilityService _facilityfervice;
+        public FacilityController(IFacilityService facilityfervice)
+        {
+            _facilityfervice = facilityfervice;
+        }
+
+
+        [HttpPost]
+        [Route("search")]
+        public async Task<IActionResult> Search( FacilitySearchRequest obj)
+        {
+            IActionResult response = Unauthorized();
+            return response;
+
+        }
+
+    }
+}
