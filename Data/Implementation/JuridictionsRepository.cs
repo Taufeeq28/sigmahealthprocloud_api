9204f2732a764593a5377f7be37ed676501ad86e
Quasif <quasif92@outlook.com>
2023-12-07 00:29:23 +0530

    removed un used name sapces

--- Data/Implementation/JuridictionsRepository.cs
+++ Data/Implementation/JuridictionsRepository.cs
@@ -1,12 +1,7 @@
-﻿using Data;
-using Data.Models;
+﻿using Data.Models;
 using Data.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Data.Implementation
 {
